2c1e8048eb20de0769e31cd6f6cf27298faca840
morsa <voxfoot@gmail.com>
2023-01-24 00:12:01 +0500

    work for project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => (
+    <main>
+        <div className="main-container flex">
+            <section className="vendor-machine">
+                regf
+            </section>
+            <section className="user-interface">
+                regreg
+            </section>
+        </div>
+    </main>
+);
+
+export default App;
